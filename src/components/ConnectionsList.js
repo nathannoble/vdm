4de5467d315f17412494d4e960a67247fe58d5f3
Nathan Noble <nathan.noble@rcggs.com>
2018-05-23 12:25:36 -0400

    Added jquery ui

--- src/components/ConnectionsList.js
+++ src/components/ConnectionsList.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 // import 'ztree/css/zTreeStyle/zTreeStyle.css'
 import 'ztree/css/metroStyle/metroStyle.css'
+import 'jqueryui/jquery-ui.css'
 
 window.jQuery = $;
 
-
 require('ztree');
-
+require('jqueryui');
 
 const setting = {}
 
@@ -16,7 +16,22 @@ class ConnectionsList extends Component {
     componentDidMount(){
         let dataSources = this.props.dataSources;
         $(document).ready(function(){
-            $.fn.zTree.init($("#treeDemo"), setting, dataSources);
+            var zTreeObj = $.fn.zTree.init($("#treeDemo"), setting, dataSources);
+            zTreeObj.expandAll(false);
+
+            $(".node_name").draggable({
+				helper : 'clone',
+				drag : function(event, ui) {
+				},
+				stop : function(event, ui) {
+				},
+				start : function(event, ui) {
+					var nodeId = $(event)[0].currentTarget.id;
+					nodeId = nodeId.substring(0, nodeId.length - 5);
+					// globals.currentnode = zTreeObj.getNodeByParam('tId', nodeId);
+					// zTreeObj.selectNode(globals.current);
+				}
+			});
          });
     }
 
