b753e38deffbca265f748f1d514f83d113276cde
Nathan Noble <nathan.noble@rcggs.com>
2018-05-30 20:39:20 -0400

    Working connections

--- src/components/Acquire.js
+++ src/components/Acquire.js
@@ -9,6 +9,7 @@ require('jqueryui');
 require('jsplumb');
 
 const jsPlumb = window.jsPlumb;
+const jsPlumbUtil = window.jsPlumbUtil;
 
 class Acquire extends Component {
 
@@ -24,66 +25,126 @@ class Acquire extends Component {
         };
       }
 
+    
+
     addNode(node, nodeKey, relX, relY, plumb){
-        function guid() {
-            function s4() {
-              return Math.floor((1 + Math.random()) * 0x10000)
-                .toString(16)
-                .substring(1);
-            }
-            return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-          }
+
+        //
+        // initialise element as connection targets and source.
+        //
+        var initNode = function(el) {
+        
+            // initialise draggable elements.
+            plumb.draggable(el);
+    
+            plumb.makeSource(el, {
+                filter: ".ep",
+                anchor: "Continuous",
+                connectorStyle: { stroke: "#5c96bc", strokeWidth: 2, outlineStroke: "transparent", outlineWidth: 4 },
+                connectionType:"basic",
+                extract:{
+                    "action":"the-action"
+                },
+                maxConnections: 2,
+                onMaxConnections: function (info, e) {
+                    alert("Maximum connections (" + info.maxConnections + ") reached");
+                }
+            });
+    
+            plumb.makeTarget(el, {
+                dropOptions: { hoverClass: "dragHover" },
+                anchor: "Continuous",
+                allowLoopback: true
+            });
+    
+        };
+    
+        var newNode = function(x, y) {
+            var d = document.createElement("div");
+            var id = jsPlumbUtil.uuid();
+            var nodeName = node.name;
+		    if (nodeName.length > 7){nodeName = nodeName.substring(0, 7) + '...';}
+            d.className = "w";
+            d.id = id;
+            d.innerHTML = nodeName + "<div class=\"ep\"></div>";
+            d.style.left = x + "px";
+            d.style.top = y + "px";
+            plumb.getContainer().appendChild(d);
+            initNode(d);
+            return d;
+        };
 
         console.log('Nodekey: ' + nodeKey);
-        var d = document.createElement("div");
-		var id = guid();
-		d.className = "w";
-		d.id = id;
-		d.style.height = '80px';
-		d.style.width = '225px';
-        d.style.padding = '5px';
+
+        newNode(relX, relY);
+
+        // var el = document.createElement("div", {id: id});
         
-        var _name_ = node.name;
-		if (_name_.length > 17){_name_ = _name_.substring(0, 17) + '...';}
+		
+		// el.className = "w";
+		// el.id = id;
+		// el.style.height = '80px';
+		// el.style.width = '225px';
+        // el.style.padding = '5px';
+        
+        // var _name_ = node.name;
+		// if (_name_.length > 17){_name_ = _name_.substring(0, 17) + '...';}
             
-        d.innerHTML = '<div>' + _name_ + '</div>'    
-
-        d.style.left = relX + "px";
-        d.style.top = relY + "px";
-
-        plumb.getContainer().appendChild(d);
+        // el.innerHTML = '<div>' + _name_ + '</div>'  
         
+        // el.style.left = relX + "px";
+        // el.style.top = relY + "px";
+
+        // plumb.getContainer().appendChild(el);
+
     }
 
     componentDidMount() {
 
         // Create an instance of jsplumb for this canvas
         let plumb = jsPlumb.getInstance({
-            PaintStyle:{ 
-              strokeWidth:6, 
-              stroke:"#567567", 
-              outlineStroke:"black", 
-              outlineWidth:1 
-            },
-            Connector:[ "Bezier", { curviness: 30 } ],
-            Endpoint:[ "Dot", { radius:5 } ],
-            EndpointStyle : { fill: "#567567"  },
-            Anchor : [ 0.5, 0.5, 1, 1 ]
-          });
+            Endpoint: ["Dot", {radius: 2}],
+            Connector:"StateMachine",
+            HoverPaintStyle: {stroke: "#1e8151", strokeWidth: 2 },
+            ConnectionOverlays: [
+                [ "Arrow", {
+                    location: 1,
+                    id: "arrow",
+                    length: 14,
+                    foldback: 0.8
+                } ],
+                [ "Label", { label: "FOO", id: "label", cssClass: "aLabel" }]
+            ],
+            Container: "canvas"
+        });
 
-          plumb.registerConnectionType("basic", {
-            anchor : "Continuous",
-            connector : "StateMachine"
+        plumb.registerConnectionType("basic", { anchor:"Continuous", connector:"StateMachine" });
+
+        var canvas = document.getElementById("canvas");
+    
+        // bind a click listener to each connection; the connection is deleted. you could of course
+        // just do this: instance.bind("click", instance.deleteConnection), but I wanted to make it clear what was
+        // happening.
+        plumb.bind("click", function (c) {
+            plumb.deleteConnection(c);
+        });
+    
+        // bind a connection listener. note that the parameter passed to this function contains more than
+        // just the new connection - see the documentation for a full list of what is included in 'info'.
+        // this listener sets the connection's internal
+        // id as the label overlay's text.
+        plumb.bind("connection", function (info) {
+            info.connection.getOverlay("label").setLabel(info.connection.id);
         });
 
         this.setState({plumb: plumb});
 
           
 
-        // fetch('http://localhost:4000/api/getconnections')
+        fetch('http://localhost:4000/api/getconnections')
         // fetch("http://localhost:4000/api/datasources")  
         // fetch("http://52.45.154.215:9290/getConnections/name")
-        fetch('http://52.45.154.215:9290/getNewConnections/name')
+        // fetch('http://52.45.154.215:9290/getNewConnections/name')
           .then(res => res.json())
           .then(
               
@@ -125,7 +186,7 @@ class Acquire extends Component {
                                         <ConnectionsList dataSources={dataSources} zTreeObj={zTreeObj} 
                                         currentNode = {currentNode} addNode={addNode}  plumb={plumb}/>
                                     </div>
-                                    <div className='canvas col-lg-6 col-md-6'><Canvas nodes={canvasNodes} plumb={plumb}/></div>
+                                    <div className='canvas'><Canvas nodes={canvasNodes} plumb={plumb}/></div>
                                     <div className='col-lg-3  col-md-3 right-pane'>Explored Datasets</div>
                                 </div>
                             </Tab>
